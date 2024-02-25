d4d4ea9465390167738e14ae2009ee45998160dc
Oracle Public Cloud User <opc@docker.subnet03121351.vcn03121351.oraclevcn.com>
2024-02-25 23:47:48 +0000

    Teste_On_Faliure

--- main.tf
+++ main.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "aws_EC2_instance" {
     tags = var.tags
     user_data = var.user_data
     provisioner "local-exec" {
-     command = "touch teste.txt"     
+     command = "touc teste.txt"
+     on_failure = fail
     }
     provisioner "local-exec" {
      command = "rm -rf teste.txt"
